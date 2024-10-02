67ef4daa4de0be27f55b35967d50b2ba8fa1f65a
Pierre Baraban <pierre@MacBook-Air-de-Pierre.local>
2024-10-02 16:49:25 +0200

    commit4

---
+++ exo5.py
@@ -0,0 +1,41 @@
+#class Personne:
+    #def __init__(self, nom):
+   #     self.nom = nom
+    
+  #  def se_presenter(self):
+ #       print(f"Bonjour, je m'appelle {self.nom}")
+
+#personne1 = Personne("Alice")
+#personne1.se_presenter()  # Bonjour, je m'appelle Alice
+
+
+
+
+#class Rectangle:
+    #def __init__(self, longueur, largeur):
+      #  self.longueur = longueur
+     #   self.largeur = largeur
+    
+    #def calculer_aire(self):
+   #     return self.longueur * self.largeur
+    
+  #  def calculer_perimetre(self):
+ #       return 2 * (self.longueur + self.largeur)
+
+
+#rectangle = Rectangle(4, 2)
+#print(rectangle.calculer_aire())  # Aire: 8
+#print(rectangle.calculer_perimetre())  # Périmètre: 12
+
+
+
+
+#class Cercle:
+    #def __init__(self, rayon):
+   #     self.rayon = rayon
+    
+  #  def calculer_aire(self):
+ #       return 3.14 * (self.rayon ** 2)
+
+#cercle = Cercle(3)
+#print(cercle.calculer_aire())  # Aire: 28.26
