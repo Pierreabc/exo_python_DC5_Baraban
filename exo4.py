054c26b097ebbae9b58ff9027e5147cfe18fdb65
Pierre Baraban <pierre@MacBook-Air-de-Pierre.local>
2024-10-02 16:47:03 +0200

    commit3

---
+++ exo4.py
@@ -0,0 +1,17 @@
+#ma_liste = [1, 2, 3, 4, 5]
+
+#ma_liste.append(6)
+
+#print(ma_liste)
+
+
+
+
+
+#mon_dictionnaire = {
+  #  "nom": "Alice",
+   # "age": 30,
+   # "profession": "Ingénieur"
+#}
+
+#print(mon_dictionnaire["age"])
